664b4f98c1bb28ec15b0020ce102cb9f6985626a
gbazin <hello@DESKTOP-8A9V9PQ.localdomain>
2025-03-01 23:30:11 +0100

    test parsing

--- sources/fractol_parsing.c
+++ sources/fractol_parsing.c
@@ -6,7 +6,7 @@
 /*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:47:53 by gbazin            #+#    #+#             */
-/*   Updated: 2025/03/01 19:31:34 by gbazin           ###   ########.fr       */
+/*   Updated: 2025/03/01 23:28:47 by gbazin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,20 +51,24 @@ static int	parse_julia_args(int argc, char **argv, t_fractol *fractol)
 	fractol->julia_param.real = ft_atod(argv[2]);
 	fractol->julia_param.imag = ft_atod(argv[3]);
 	fractol->fractal_type = JULIA;
-	if (argc >= 5 && ft_strncmp(argv[4], "bw", 3) == 0)
+	if (argc == 5 && ft_strncmp(argv[4], "bw", 3) == 0)
 		fractol->color_scheme = BLACK_WHITE;
-	else
+	else if (argc == 5 && ft_strncmp(argv[4], "color", 6) == 0)
 		fractol->color_scheme = COLORFUL;
+	else
+		return (0);
 	return (1);
 }
 
 static int	parse_mandelbrot_args(int argc, char **argv, t_fractol *fractol)
 {
 	fractol->fractal_type = MANDELBROT;
-	if (argc >= 3 && ft_strncmp(argv[2], "bw", 3) == 0)
+	if (argc == 3 && ft_strncmp(argv[2], "bw", 3) == 0)
 		fractol->color_scheme = BLACK_WHITE;
-	else
+	else if (argc == 3 && ft_strncmp(argv[2], "color", 6) == 0)
 		fractol->color_scheme = COLORFUL;
+	else
+		return (0);
 	return (1);
 }
 
